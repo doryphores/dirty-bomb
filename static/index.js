11fee718f1cd6fe499e0545a50f45e29523367e1
Martin Laine <martin.laine@gmail.com>
2015-03-22 23:56:02 +0000

    Add stylus

--- static/index.js
+++ static/index.js
@@ -1,5 +1,15 @@
 require("node-jsx").install({extension: ".jsx"});
+var fs = require("fs")
+var stylus = require("stylus");
 
 window.onload = function () {
+  fs.readFile(__dirname + "/css/test.styl", {encoding: "utf-8"}, function (err, styles) {
+    console.log(err);
+    stylus(styles).set("filename", "test.styl").render(function (err, css) {
+      var s = document.createElement("style");
+      s.innerHTML = css;
+      document.querySelector("head").appendChild(s);
+    });
+  });
   require("../src/components/ui");
 };
