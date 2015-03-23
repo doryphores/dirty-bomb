eda838b86237315250db31cdc6f424fa26889c0f
Martin Laine <martin.laine@simplybusiness.co.uk>
2015-03-23 17:08:03 +0000

    Convert theme to stylus

--- static/index.js
+++ static/index.js
@@ -1,15 +1,30 @@
 require("node-jsx").install({extension: ".jsx"});
-var fs = require("fs")
-var stylus = require("stylus");
+
+var fs     = require("fs"),
+    path   = require("path"),
+    stylus = require("stylus");
 
 window.onload = function () {
-  fs.readFile(__dirname + "/css/test.styl", {encoding: "utf-8"}, function (err, styles) {
-    console.log(err);
-    stylus(styles).set("filename", "test.styl").render(function (err, css) {
-      var s = document.createElement("style");
-      s.innerHTML = css;
-      document.querySelector("head").appendChild(s);
-    });
+  compileCSS(function () {
+    require("../src/components/ui");
   });
-  require("../src/components/ui");
 };
+
+function compileCSS(done) {
+  console.time("Compile CSS");
+  var themeFilename = path.join(__dirname, "theme.styl");
+  fs.readFile(themeFilename, {encoding: "utf-8"}, function (err, styles) {
+    if (err) throw err;
+    stylus(styles)
+      .set("filename", themeFilename)
+      .set("paths", [__dirname])
+      .render(function (err, css) {
+        if (err) throw err;
+        var s = document.createElement("style");
+        s.innerHTML = css;
+        document.querySelector("head").appendChild(s);
+        console.timeEnd("Compile CSS");
+        done();
+      });
+  });
+}
