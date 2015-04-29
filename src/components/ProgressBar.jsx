b6868693023fa1933bafbe626283a1156ed8bb95
Martin Laine <martin.laine@gmail.com>
2015-04-29 22:50:57 +0100

    Style progress bar and add label

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,10 +1,14 @@
-var React = require('react');
+var React = require("react");
 
 var ProgressBar = React.createClass({
   render: function() {
+    var width = (this.props.progress || 0) + "%";
     return (
       <div className="progress">
         <div className="progress__bar" style={{width: this.props.progress + "%"}} />
+        <div className="progress__label">
+          {this.props.label + (this.props.progress ? " - " + width : "")}
+        </div>
       </div>
     );
   }
