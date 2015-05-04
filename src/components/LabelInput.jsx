faedb13468621e048bbba995649790049458db1a
Martin Laine <martin.laine@gmail.com>
2015-05-04 16:46:03 +0100

    Move LabelInput to its own file

---
+++ src/components/LabelInput.jsx
@@ -0,0 +1,53 @@
+var React = require("react");
+
+var LabelInput = React.createClass({
+  getInitialState: function () {
+    return {
+      filename: this.props.filename
+    };
+  },
+
+  componentDidMount: function() {
+    var inputElement = this.refs.input.getDOMNode();
+    var extIndex = this.props.filename.lastIndexOf(".md");
+    inputElement.setSelectionRange(0,
+      extIndex > -1 ? extIndex : this.props.filename.length);
+    inputElement.focus();
+  },
+
+  render: function () {
+    return (
+      <div className="label-input">
+        <input ref="input"
+            value={this.state.filename}
+            onChange={this._onChange}
+            onKeyUp={this._onKeyUp}
+            onBlur={this._onBlur} />
+      </div>
+    )
+  },
+
+  _onChange: function (e) {
+    this.setState({filename: e.currentTarget.value});
+  },
+
+  _onKeyUp: function (e) {
+    switch (e.key) {
+      case "Enter":
+        var formatted = this.state.filename.toLowerCase().replace(/\s+/g, "-");
+        this.props.onChange(formatted);
+        break;
+      case "Escape":
+        this.props.onChange(this.props.filename);
+        break;
+      default:
+        // no op
+    }
+  },
+
+  _onBlur: function () {
+    this.props.onChange(this.props.filename);
+  }
+});
+
+module.exports = LabelInput;
--- src/components/Tree.jsx
+++ src/components/Tree.jsx
@@ -8,12 +8,10 @@ var React           = require("react"),
     EditorActions   = require("../actions/EditorActions"),
     path            = require("path"),
     remote          = require("remote"),
-    Menu            = remote.require("menu");
+    Menu            = remote.require("menu"),
+    LabelInput      = require("./LabelInput");
 
 
-/*=============================================*\
-  Component definitions
-\*=============================================*/
 
 var Tree = module.exports = React.createClass({
   mixins: [PureRenderMixin, Reflux.connect(TreeStore, "tree")],
@@ -114,12 +112,9 @@ Tree.Node = React.createClass({
           <ul className="tree__node-list">
             {this.props.node.get("children").map(function (node) {
               return (
-                <Tree.Node
-                  key={node.get("name")}
-                  node={node}
-                  fs={this.props.fs} />
+                <Tree.Node key={node.get("name")} node={node} />
               );
-            }.bind(this))}
+            })}
           </ul>
         </li>
       );
@@ -209,55 +204,5 @@ Tree.Node = React.createClass({
       TreeActions.rename(this.props.node.get("path"), filename);
     }
     this.setState({editMode: false});
-  }
-});
-
-var LabelInput = React.createClass({
-  getInitialState: function () {
-    return {
-      filename: this.props.filename
-    };
-  },
-
-  componentDidMount: function() {
-    var inputElement = this.refs.input.getDOMNode();
-    var extIndex = this.props.filename.lastIndexOf(".md");
-    inputElement.setSelectionRange(0,
-      extIndex > -1 ? extIndex : this.props.filename.length);
-    inputElement.focus();
-  },
-
-  render: function () {
-    return (
-      <div className="label-input">
-        <input ref="input"
-            value={this.state.filename}
-            onChange={this._onChange}
-            onKeyUp={this._onKeyUp}
-            onBlur={this._onBlur} />
-      </div>
-    )
-  },
-
-  _onChange: function (e) {
-    this.setState({filename: e.currentTarget.value});
-  },
-
-  _onKeyUp: function (e) {
-    switch (e.key) {
-      case "Enter":
-        var formatted = this.state.filename.toLowerCase().replace(/\s+/g, "-");
-        this.props.onChange(formatted);
-        break;
-      case "Escape":
-        this.props.onChange(this.props.filename);
-        break;
-      default:
-        // no op
-    }
-  },
-
-  _onBlur: function () {
-    this.props.onChange(this.props.filename);
   }
 });
